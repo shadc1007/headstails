514f45756e6af2ea9543f20a023ed5c8d198a9e7
Shadrack V. Castro <castroshadrack7@gmail.com>
2023-04-08 16:22:30 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+// initialize computer pick
+// compare computer pick and user pick
+// display the result and save
+
+let scoreBoard = JSON.parse(localStorage.getItem('score')) || {
+    wins: 0,
+    loses: 0
+}
+
+let computerPick = ''
+let result = ''
+let wins = document.getElementById('wins');
+let loses = document.getElementById('loses');
+// retrieve
+wins.innerText = `Wins: ${scoreBoard.wins}`
+loses.innerText = `Loses: ${scoreBoard.loses}`
+
+const pickOfComputer = () => {
+    let randomNumber = Math.random()
+    if (randomNumber > 0.5) {
+        computerPick = 'Heads'
+    } else {
+        computerPick = 'Tails'
+    }
+}
+
+const evaluateResult = (user) => {
+    if (user === computerPick) {
+        result = 'You guess correctly.'
+        scoreBoard.wins += 1;
+    } else {
+        result = 'You guess the wrong one.'
+        scoreBoard.loses += 1;
+    }   
+
+   alert(`You picked ${user}. Computer picked ${computerPick}. ${result}`)
+   displayResult()
+   localStorage.setItem('score', JSON.stringify(scoreBoard))
+}
+
+const displayResult = () => {
+    wins.innerText = `Wins: ${scoreBoard.wins}`
+    loses.innerText = `Loses: ${scoreBoard.loses}`
+    localStorage.setItem('score', JSON.stringify(scoreBoard))
+}
+
+const resetScores = () => {
+    alert('Scoreboard has been reset.')
+    scoreBoard = {
+        wins: 0,
+        loses: 0
+    }
+    wins.innerText = `Wins: ${scoreBoard.wins}`
+    loses.innerText = `Loses: ${scoreBoard.loses}`
+    localStorage.removeItem('score')
+}
